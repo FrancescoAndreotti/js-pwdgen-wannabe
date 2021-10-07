5c563a5f151313efb7bf26d7f75590f588b70e85
FrancescoAndreotti <francescoandreotti00@gmail.com>
2021-10-07 17:35:35 +0200

    Finito

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let nomeUtente = prompt("Qual'è il tuo nome?")
+let cognomeUtente = prompt("Qual'è il tuo cognome?")
+let colorePreferito = prompt("Qual'è il tuo colore preferito?")
+alert(nomeUtente + cognomeUtente + colorePreferito + "21")
